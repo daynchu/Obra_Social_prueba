eec8be1c4881d6bd6079376bd255f2f5da491df9
David <dbaigorria@excelenciadigital.net>
2019-11-28 14:52:08 -0300

    conexion

---
+++ build/precache-manifest.7cd21c9f8d6f6d45a91b1ea6dcd18e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905d0562de6e5c2acef5491f1f2ef941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6db5da98675719d7df0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "351dd3d922ce8ce7c602",
+    "url": "/static/js/2.75f33810.chunk.js"
+  },
+  {
+    "revision": "c6db5da98675719d7df0",
+    "url": "/static/js/main.6d989e5d.chunk.js"
+  },
+  {
+    "revision": "3cbe6e9b71056fafeb8b",
+    "url": "/static/js/runtime-main.530255f7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
